4bcf4940c6bbf40e1eade32422d4e4acfe8e763c
ruchap24 <160455987+ruchap24@users.noreply.github.com>
2024-11-09 09:50:39 +0530

    Time: 1 ms (87.5%), Space: 40.5 MB (79.81%) - LeetHub

---
+++ 3394-minimum-array-end/3394-minimum-array-end.java
@@ -0,0 +1,49 @@
+class Solution {
+    public long minEnd(int n, int x) {
+        String binaryString = Integer.toBinaryString(n - 1);
+        String startString = Integer.toBinaryString(x);
+
+        int a = startString.length() - 1;
+        int b = binaryString.length() - 1;
+        StringBuilder sBuilder = new StringBuilder();
+
+        while (true) {
+            if (a == -1 && b == -1) {
+                break;
+            } else {
+                if (a == -1) {
+                    while (b != -1) {
+                        sBuilder.insert(0, binaryString.charAt(b));
+                        b--;
+                    }
+                    break;
+                } else if (b == -1) {
+                    while (a != -1) {
+                        sBuilder.insert(0, startString.charAt(a));
+                        a--;
+                    }
+                } else {
+                    char atStart = startString.charAt(a);
+                    char atBin = binaryString.charAt(b);
+
+                    if (atStart == '1') {
+                        sBuilder.insert(0, atStart);
+                        a--;
+                    } else {
+                        sBuilder.insert(0, atBin);
+                        a--;
+                        b--;
+                    }
+                }
+            }
+        }
+
+        long result = 0;
+        int length = sBuilder.length();
+        for (int i = 0; i < length; i++) {
+            result = result * 2 + (sBuilder.charAt(i) - '0');
+        }
+
+        return result;
+    }
+}
