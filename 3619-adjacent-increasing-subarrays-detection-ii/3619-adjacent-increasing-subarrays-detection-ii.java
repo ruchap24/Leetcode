349c12a3591867f9665488ea35aa8aca380b3413
ruchap24 <160455987+ruchap24@users.noreply.github.com>
2024-11-11 16:58:22 +0530

    Time: 25 ms (100%), Space: 91 MB (66.67%) - LeetHub

--- 3619-adjacent-increasing-subarrays-detection-ii/3619-adjacent-increasing-subarrays-detection-ii.java
+++ 3619-adjacent-increasing-subarrays-detection-ii/3619-adjacent-increasing-subarrays-detection-ii.java
@@ -1,29 +1,27 @@
 class Solution {
-    public boolean solve(int prefix[], int n, int k) {
-
-        for (int i = 0; i+2*k-1 < n; i++) {
-            if (prefix[i+k-1]-prefix[i] == k-1 && prefix[i+2*k-1]-prefix[i+k] == k-1) return true;
-        }
-        return false;
-    }
     public int maxIncreasingSubarrays(List<Integer> nums) {
-        int n = nums.size();
-        int prefix[] = new int[n];
+        int prefixIncreasing[] = new int[nums.size()];
 
-        for (int i = 1; i < n; i++) {
-            prefix[i] = prefix[i-1] + ((nums.get(i)>nums.get(i-1)) ? 1 : 0);
-        }
-        int low = 1, high = n/2, ans = 1;
-
-        while (low <= high ) {
-            int mid = low+(high-low)/2;
-            if (solve(prefix, n, mid)) {
-                ans = mid;
-                low = mid+1;
-            } else {
-                high = mid-1;
+        prefixIncreasing[0] = 1;
+        for(int i=1;i<nums.size();i++){
+            if(nums.get(i)>nums.get(i-1)){
+                prefixIncreasing[i] = prefixIncreasing[i-1]+1;
+            }
+            else{
+                prefixIncreasing[i] = 1;
             }
         }
-        return ans;
+
+        int res = 1;
+        for(int i=1;i<nums.size();i++){
+            int k = prefixIncreasing[i];
+            if(k>1 && i-k>=0 && prefixIncreasing[i-k]>=k){
+                res = Math.max(res,k);
+            }
+            if(prefixIncreasing[i]%2==0){
+                res = Math.max(res,prefixIncreasing[i]/2);
+            }
+        }
+        return res;
     }
 }
