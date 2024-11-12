08231d522d8c31b2b7bbde8429f5eed8988b07df
ruchap24 <160455987+ruchap24@users.noreply.github.com>
2024-11-12 11:58:18 +0530

    Time: 4 ms (100%), Space: 44.5 MB (9.21%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -3,11 +3,12 @@ class Solution{
         if(x<0 || (x%10==0 && x!=0)){
             return false;
         }
+        int n=x;
         int rev=0;
-        while(x>rev){
-            rev=rev*10 + x%10;
+        while(x>0){
+            rev=rev*10+x%10;
             x=x/10;
         }
-        return x==rev || x==rev/10;
+        return n==rev?true:false;
     }
 }
