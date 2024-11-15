b26845c162ea79530df1eaf8e7b6234b00af6c1a
ruchap24 <160455987+ruchap24@users.noreply.github.com>
2024-11-15 13:49:40 +0530

    Time: 4 ms (13.97%), Space: 44.3 MB (5.09%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.java
+++ 0090-subsets-ii/0090-subsets-ii.java
@@ -5,19 +5,16 @@ class Solution {
         subsetSums(nums, 0, new ArrayList<>(), result);
         return result.stream().distinct().collect(Collectors.toList());
     }
-    
     public void subsetSums(int[] arr, int index, List<Integer> curr, List<List<Integer>> result) {
         if(index == arr.length) {
             List<Integer> temp = new ArrayList<>();
             temp.addAll(curr);
             result.add(temp);
             return;
-        }
-        
+        } 
         subsetSums(arr, index + 1, curr, result);
         curr.add(arr[index]);
         subsetSums(arr, index + 1, curr, result);
         curr.remove(curr.size() - 1);
     }
-    
 }
