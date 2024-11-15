003bce13626b1b1e8ee5242fa4cb9fae55ffea33
ruchap24 <160455987+ruchap24@users.noreply.github.com>
2024-11-15 13:48:52 +0530

    Time: 3 ms (22.44%), Space: 44.4 MB (5.09%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.java
+++ 0090-subsets-ii/0090-subsets-ii.java
@@ -1,24 +1,23 @@
-public class Solution {
+class Solution {
     public List<List<Integer>> subsetsWithDup(int[] nums) {
-        int n = nums.length;
-        List<List<Integer>> ans = new ArrayList<>();
-        List<Integer> set = new ArrayList<>();
+        List<List<Integer>> result = new ArrayList<>();
         Arrays.sort(nums);
-        
-        backtrack(nums, n, ans, set, 0);
-        return ans;
+        subsetSums(nums, 0, new ArrayList<>(), result);
+        return result.stream().distinct().collect(Collectors.toList());
     }
-    private void backtrack(int[] nums, int n, List<List<Integer>> ans, List<Integer> set, int idx) {
-        if (idx >= n) {
-            ans.add(new ArrayList<>(set));
+    
+    public void subsetSums(int[] arr, int index, List<Integer> curr, List<List<Integer>> result) {
+        if(index == arr.length) {
+            List<Integer> temp = new ArrayList<>();
+            temp.addAll(curr);
+            result.add(temp);
             return;
-        } 
-        set.add(nums[idx]);
-        backtrack(nums, n, ans, set, idx + 1);
-        set.remove(set.size() - 1);
-        while (idx + 1 < n && nums[idx] == nums[idx + 1]) {
-            idx++;
         }
-        backtrack(nums, n, ans, set, idx + 1);
+        
+        subsetSums(arr, index + 1, curr, result);
+        curr.add(arr[index]);
+        subsetSums(arr, index + 1, curr, result);
+        curr.remove(curr.size() - 1);
     }
+    
 }
