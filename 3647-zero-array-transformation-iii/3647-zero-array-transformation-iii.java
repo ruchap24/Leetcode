71951ef074049c38a82def365cf51e23435b114d
ruchap24 <160455987+ruchap24@users.noreply.github.com>
2024-11-24 19:56:06 +0530

    Time: 455 ms (66.67%), Space: 91.5 MB (66.67%) - LeetHub

--- 3647-zero-array-transformation-iii/3647-zero-array-transformation-iii.java
+++ 3647-zero-array-transformation-iii/3647-zero-array-transformation-iii.java
@@ -1,6 +1,5 @@
 class Solution {
     public int maxRemoval(int[] nums, int[][] queries) {
-        // Custom comparator for reverse order
         TreeSet<int[]> s1 = new TreeSet<>((a, b) -> {
             if (a[0] != b[0]) return b[0] - a[0];
             if (a[1] != b[1]) return b[1] - a[1];
@@ -11,31 +10,34 @@ class Solution {
             if (a[1] != b[1]) return b[1] - a[1];
             return b[2] - a[2];
         });  
-        // Maps to store query indices
+
         Map<Integer, List<Integer>> m1 = new HashMap<>();
         Map<Integer, List<Integer>> m2 = new HashMap<>(); 
-        // Populate maps
+ 
         for (int i = 0; i < queries.length; i++) {
             m1.computeIfAbsent(queries[i][0], k -> new ArrayList<>()).add(i);
             m2.computeIfAbsent(queries[i][1], k -> new ArrayList<>()).add(i);
         }
         int cnt = 0, res = 0;
-        // Process each number
+  
         for (int i = 0; i < nums.length; i++) {
-            // Add new queries starting at current position
+    
             if (m1.containsKey(i)) {
                 for (int x : m1.get(i)) {
-                    s1.add(new int[]{queries[x][1], i, x});} }
-            // Process queries while we need more elements
+                    s1.add(new int[]{queries[x][1], i, x});
+                    } 
+                }
+        
             while (cnt < nums[i] && !s1.isEmpty()) {
                 s2.add(s1.first());
                 res++;
                 cnt++;
-                s1.remove(s1.first()); }
-            // Check if we couldn't satisfy the requirement
+                s1.remove(s1.first()); 
+                }
+   
             if (cnt < nums[i]) {
                 return -1; }
-            // Remove ended queries
+       
             if (m2.containsKey(i)) {
                 for (int x : m2.get(i)) {
                     int[] query = new int[]{i, queries[x][0], x};
@@ -44,7 +46,9 @@ class Solution {
                     }
                     s2.remove(query);
                     s1.remove(query);
-                } } }
+                }
+            } 
+        }
         return queries.length - res;
     }
 }
