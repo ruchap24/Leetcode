b1e345cc6fa9712aa3700c62ae8343a763ac5848
ruchap24 <160455987+ruchap24@users.noreply.github.com>
2024-11-24 19:57:51 +0530

    Time: 13 ms (100%), Space: 141.8 MB (33.33%) - LeetHub

--- 3648-find-the-maximum-number-of-fruits-collected/3648-find-the-maximum-number-of-fruits-collected.java
+++ 3648-find-the-maximum-number-of-fruits-collected/3648-find-the-maximum-number-of-fruits-collected.java
@@ -1,7 +1,7 @@
 class Solution {
     public int maxCollectedFruits(int[][] A) {
         int n = A.length;
-        // Set inaccessible cells to 0
+    
         for (int i = 0; i < n; ++i) {
             for (int j = 0; j < n; ++j) {
                 if (i < j && j < n - 1 - i) {
