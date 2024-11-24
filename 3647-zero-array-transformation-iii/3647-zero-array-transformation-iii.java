af26f9177e156bba3d6c572a437313dd13b669bc
ruchap24 <160455987+ruchap24@users.noreply.github.com>
2024-11-24 19:54:26 +0530

    Time: 457 ms (66.67%), Space: 91.4 MB (66.67%) - LeetHub

--- 3647-zero-array-transformation-iii/3647-zero-array-transformation-iii.java
+++ 3647-zero-array-transformation-iii/3647-zero-array-transformation-iii.java
@@ -1,51 +1,50 @@
 class Solution {
     public int maxRemoval(int[] nums, int[][] queries) {
-        // need to iterate over queries with starting point asc
-        Arrays.sort(queries, (a, b) -> a[0] == b[0] ? b[1] - a[1] : a[0] - b[0]);
-        int[] effect = new int[nums.length + 1];
-        int res = 0;
-        // here we store active queries which we can use to cover nums[i]
-        Queue<int[]> pq = new PriorityQueue<>((a, b) -> b[1] - a[1]);
-        int j = 0; // queries index
-        // now for each index
-        for (int i = 0; i < nums.length; i++) {
-            // queries we can no longer use
-            while (!pq.isEmpty() && pq.peek()[1] < i) {
-                pq.poll();
-            }
-            // same here, moving j over unused queries
-            while (j < queries.length && queries[j][1] < i) {
-                j++;
-            }
-            // while our cumulative effect is less than the effect we need
-            while (effect[i] < nums[i]) {
-                boolean used = false;
-                // all the queries we can potentially use go to queue
-                while (j < queries.length && queries[j][0] <= i) {
-                    pq.add(queries[j]);
-                    j++;
-                }
-                // why we can use some query
-                if (!pq.isEmpty()) {
-                    int[] tmp = pq.poll();
-                    // can't use it - went over its ending
-                    if (tmp[1] < i) {
-                        continue; }
-                    // now let's add cumulative effect
-                    effect[i]++;
-                    // at this point query ends and we need to take back the effect
-                    effect[tmp[1] + 1]--;
-                    res++; // using the query
-                    used = true;
-                }
-                // if we can't cover the i - we're out
-                if (!used) {
-                    return -1;}
-            }
-            // works as prefix sum to cover the ending queries
-            effect[i + 1] += effect[i];
+        // Custom comparator for reverse order
+        TreeSet<int[]> s1 = new TreeSet<>((a, b) -> {
+            if (a[0] != b[0]) return b[0] - a[0];
+            if (a[1] != b[1]) return b[1] - a[1];
+            return b[2] - a[2];
+        });
+        TreeSet<int[]> s2 = new TreeSet<>((a, b) -> {
+            if (a[0] != b[0]) return b[0] - a[0];
+            if (a[1] != b[1]) return b[1] - a[1];
+            return b[2] - a[2];
+        });  
+        // Maps to store query indices
+        Map<Integer, List<Integer>> m1 = new HashMap<>();
+        Map<Integer, List<Integer>> m2 = new HashMap<>(); 
+        // Populate maps
+        for (int i = 0; i < queries.length; i++) {
+            m1.computeIfAbsent(queries[i][0], k -> new ArrayList<>()).add(i);
+            m2.computeIfAbsent(queries[i][1], k -> new ArrayList<>()).add(i);
         }
-        // everything unused is what we can throw off
+        int cnt = 0, res = 0;
+        // Process each number
+        for (int i = 0; i < nums.length; i++) {
+            // Add new queries starting at current position
+            if (m1.containsKey(i)) {
+                for (int x : m1.get(i)) {
+                    s1.add(new int[]{queries[x][1], i, x});} }
+            // Process queries while we need more elements
+            while (cnt < nums[i] && !s1.isEmpty()) {
+                s2.add(s1.first());
+                res++;
+                cnt++;
+                s1.remove(s1.first()); }
+            // Check if we couldn't satisfy the requirement
+            if (cnt < nums[i]) {
+                return -1; }
+            // Remove ended queries
+            if (m2.containsKey(i)) {
+                for (int x : m2.get(i)) {
+                    int[] query = new int[]{i, queries[x][0], x};
+                    if (s2.contains(query)) {
+                        cnt--;
+                    }
+                    s2.remove(query);
+                    s1.remove(query);
+                } } }
         return queries.length - res;
     }
 }
