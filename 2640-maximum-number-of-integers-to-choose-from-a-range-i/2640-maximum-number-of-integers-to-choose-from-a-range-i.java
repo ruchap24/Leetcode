40550eee4152cee3cf6bd182aeadf1700b615669
ruchap24 <160455987+ruchap24@users.noreply.github.com>
2024-12-06 21:29:46 +0530

    Time: 8 ms (90.22%), Space: 45.7 MB (60.45%) - LeetHub

--- 2640-maximum-number-of-integers-to-choose-from-a-range-i/2640-maximum-number-of-integers-to-choose-from-a-range-i.java
+++ 2640-maximum-number-of-integers-to-choose-from-a-range-i/2640-maximum-number-of-integers-to-choose-from-a-range-i.java
@@ -1,21 +1,17 @@
 class Solution {
     public int maxCount(int[] banned, int n, int maxSum) {
-        int[] arr=new int[10001];
-        for(int i=0; i<banned.length; i++){
-            arr[banned[i]]=1;
-        }
-
-        long sum=0;
-        int cnt=0;
-        for(int i=1; i<=n; i++){
-            if(arr[i]==1){continue;}
-            sum+=i;
-            if(sum>maxSum){
-               break;
+        int res=0;
+        int sum=0;
+        int a[]=new int[100001];
+        for(int i:banned)a[i]++;
+        for(int i=1;i<=n;i++)
+        {
+            if(a[i]==0&&(i+sum)<=maxSum){
+                res++;
+                sum+=i;
             }
-            cnt++;
         }
-
-        return cnt;
+        return res;   
     }
+          
 }
