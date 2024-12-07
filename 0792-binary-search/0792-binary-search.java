ed47b95b81b70eb70c9f4bb42203905ef04005bf
ruchap24 <160455987+ruchap24@users.noreply.github.com>
2024-12-07 19:25:22 +0530

    Time: 0 ms (100%), Space: 46.2 MB (5.46%) - LeetHub

---
+++ 0792-binary-search/0792-binary-search.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int s=0;
+        int e=nums.length-1;
+        while(s<=e){
+            int mid=(s+e)/2;
+        if(target==nums[mid]){
+            return mid;
+        }
+        else if(target>nums[mid]){
+            s=mid+1;
+        }
+        else{
+            e=mid-1;
+        }
+     }
+     return -1;
+    }
+}
