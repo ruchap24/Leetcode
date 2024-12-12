47d0b627379e0807e02e032831b8970fc8d58866
ruchap24 <160455987+ruchap24@users.noreply.github.com>
2024-12-12 15:00:10 +0530

    Time: 2 ms (90.24%), Space: 43.1 MB (61.72%) - LeetHub

--- 1468-check-if-n-and-its-double-exist/1468-check-if-n-and-its-double-exist.java
+++ 1468-check-if-n-and-its-double-exist/1468-check-if-n-and-its-double-exist.java
@@ -1,3 +1,18 @@
+public class Solution {
+    public boolean checkIfExist(int[] arr) {
+        HashSet<Integer> set = new HashSet<>();
+        for (int num : arr) {
+            if (set.contains(num * 2) || (num % 2 == 0 && set.contains(num / 2))) {
+                return true;
+            }
+            set.add(num);
+        }
+        return false;
+    }
+}  //O(n)
+
+
+
 // O(n^2)
 
 // class Solution {
@@ -14,21 +29,43 @@
 //     }
 // }
 
-class Solution {
-    public boolean checkIfExist(int[] arr) {
+
+// O(n)
+// class Solution {
+//     public boolean checkIfExist(int[] arr) {
         
-        int len = arr.length;
-        HashSet<Integer> set = new HashSet<>();
+//         int len = arr.length;
+//         HashSet<Integer> set = new HashSet<>();
 
-        for (int i = 0; i < len; i++) {
+//         for (int i = 0; i < len; i++) {
 
-            int curr = arr[i];
+//             int curr = arr[i];
 
-            if (set.contains(2 * curr) || (curr % 2 == 0 && set.contains(curr / 2)))
-                return true;
-            set.add(arr[i]);
-        }
+//             if (set.contains(2 * curr) || (curr % 2 == 0 && set.contains(curr / 2)))
+//                 return true;
+//             set.add(arr[i]);
+//         }
 
-        return false;
-    }
-}
+//         return false;
+//     }
+// }
+
+//O(n)
+// class Solution {
+//     public boolean checkIfExist(int[] arr) {
+//         List<Integer> l= new ArrayList<>();
+//         Set<Integer> s= new HashSet<>();
+//         for(int i: arr){
+//             l.add(i);
+//             s.add(i);
+//         }
+//         for(int i: s) {
+//             if(i==0){
+//                 if(Collections.frequency(l, 0)>1) return true;
+//                 continue;
+//             }
+//             if(s.contains(i*2)) return true;
+//         }
+//         return false;
+//     }
+// }
