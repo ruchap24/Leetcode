1ff9e6e1cebf8fa1633c7992e3a3c021282e0ac6
ruchap24 <160455987+ruchap24@users.noreply.github.com>
2024-12-14 10:42:48 +0530

    Time: 0 ms (100%), Space: 40.6 MB (32.24%) - LeetHub

--- 0633-sum-of-square-numbers/0633-sum-of-square-numbers.java
+++ 0633-sum-of-square-numbers/0633-sum-of-square-numbers.java
@@ -1,13 +1,53 @@
+// class Solution {
+//     public boolean judgeSquareSum(int c) {
+//         long a = 0, b = (long) Math.sqrt(c);  // c<=2^31-1
+//         while (a <= b) {
+//             if (a * a + b * b == c) return true;
+//             else if (a * a + b * b > c) b--;
+//             else a++;
+//         }
+//         return false;
+//     }
+// }
+
+//fermat's theorem states that a number can be represented as the sum of two squares if and only if all prime factors of the form ( 4k + 3 ) have even powers in its prime factorization.
 class Solution {
     public boolean judgeSquareSum(int c) {
-        long a = 0, b = (long) Math.sqrt(c);  // c<=2^31-1
-        while (a <= b) {
-            if (a * a + b * b == c) return true;
-            else if (a * a + b * b > c) b--;
-            else a++;
+        for (int divisor = 2; divisor * divisor <= c; divisor++) {
+            if (c % divisor == 0) {
+                int exponentCount = 0;
+                while (c % divisor == 0) {
+                    exponentCount++;
+                    c /= divisor;
+                }
+                if (divisor % 4 == 3 && exponentCount % 2 != 0) {
+                    return false;
+                }
+            }
         }
-        return false;
+        return c % 4 != 3;
     }
 }
 
-//fermat's theorem
+
+
+// class Solution {
+//     public boolean judgeSquareSum(int c) {
+//         if (c%4==3) return false;
+
+//         int i=2, j=(int)Math.sqrt(c);
+//         while (i<=j){
+//             if (c%i==0){
+//                 c/=i;
+//                 int cnt=1;
+//                 while (c%i==0){
+//                     cnt++;
+//                     c/=i;
+//                 }
+//                 if (i%4==3 && cnt%2!=0) return false;
+//             }
+//             i++;
+//         }
+//         return c%4!=3;
+//     }
+// }
