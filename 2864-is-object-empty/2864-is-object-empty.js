db3cb828752939cef588fcdde3405acd3869c060
ruchap24 <160455987+ruchap24@users.noreply.github.com>
2024-12-17 14:56:08 +0530

    Time: 52 ms (55.85%), Space: 48.5 MB (97.77%) - LeetHub

---
+++ 2864-is-object-empty/2864-is-object-empty.js
@@ -0,0 +1,20 @@
+// /**
+//  * @param {Object|Array} obj
+//  * @return {boolean}
+//  */
+var isEmpty = function(obj) {
+    for(let key in obj){
+        return false;
+    }
+    return true;
+};
+
+// using Object.keys
+// var isEmpty=function(onj){
+//     if(Object.keys(obj).length===0){
+//         return true;
+//     }
+//     else{
+//         return false;
+//     }
+// };
