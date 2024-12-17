26ee4e42ad0c518f896106d2d394e535900977c1
ruchap24 <160455987+ruchap24@users.noreply.github.com>
2024-12-17 14:58:39 +0530

    Time: 56 ms (32.38%), Space: 49.4 MB (62.96%) - LeetHub

--- 2864-is-object-empty/2864-is-object-empty.js
+++ 2864-is-object-empty/2864-is-object-empty.js
@@ -10,11 +10,22 @@
 // };
 
 // using Object.keys
-var isEmpty=function(obj){
-    if(Object.keys(obj).length===0){
-        return true;
-    }
-    else{
-        return false;
-    }
+// var isEmpty=function(obj){
+//     if(Object.keys(obj).length===0){
+//         return true;
+//     }
+//     else{
+//         return false;
+//     }
+// };
+
+
+const isEmpty = function(obj) {
+    if (obj === null) return true; 
+
+    if (Array.isArray(obj)) return obj.length === 0; 
+
+    if (typeof obj === "object") return Object.keys(obj).length === 0;
+
+    return false
 };
