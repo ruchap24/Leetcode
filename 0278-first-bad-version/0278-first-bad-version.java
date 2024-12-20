8160c3357b8ee508d0addc64ccb3ab508cfc5917
ruchap24 <160455987+ruchap24@users.noreply.github.com>
2024-12-20 15:13:17 +0530

    Time: 22 ms (98.24%), Space: 40.7 MB (10.71%) - LeetHub

--- 0278-first-bad-version/0278-first-bad-version.java
+++ 0278-first-bad-version/0278-first-bad-version.java
@@ -1,18 +1,37 @@
 /* The isBadVersion API is defined in the parent class VersionControl.
       boolean isBadVersion(int version); */
 
+// public class Solution extends VersionControl {
+//     public int firstBadVersion(int n) {
+//         int s=1, e=n;
+//         while(s<e){
+//             int m=s+(e-s)/2;
+//         if(isBadVersion(m)){
+//             e=m;
+//            }
+//         else{
+//             s=m+1;
+//            }
+//         }
+//         return s;
+//     }
+// }
+
+
 public class Solution extends VersionControl {
     public int firstBadVersion(int n) {
-        int s=1, e=n;
-        while(s<e){
-            int m=s+(e-s)/2;
-        if(isBadVersion(m)){
-            e=m;
-           }
-        else{
-            s=m+1;
-           }
-        }
-        return s;
+        return recFirstBadVersion(1, n);
+    }
+
+    public int recFirstBadVersion(int b, int e) {
+        if (b >= e)
+            return b;
+
+        int mid = b + (e - b) / 2;
+
+        if (isBadVersion(mid))
+            return recFirstBadVersion(b, mid);
+        else
+            return recFirstBadVersion(mid + 1, e);  
     }
 }
