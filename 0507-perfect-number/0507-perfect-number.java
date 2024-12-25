a1f833bb383ada87ef5a717d5951f9ecf518d662
Rucha <160455987+ruchap24@users.noreply.github.com>
2024-12-25 16:07:41 +0530

    Time: 242 ms (74.12%), Space: 44.5 MB (8.8%) - LeetHub

--- 0507-perfect-number/0507-perfect-number.java
+++ 0507-perfect-number/0507-perfect-number.java
@@ -1,10 +1,38 @@
+// class Solution {
+//     public boolean checkPerfectNumber(int n) {
+//      if(num%2!=0 || num<5){
+//             return false;
+//         }
+//         int sum=0;
+//         for(int i=1;i<=n/2;i++){
+//             if(n%i==0) sum+=i;
+//         }
+//         if(n==sum) return true;
+//         return false;
+//     }
+// }
+
+
 class Solution {
-    public boolean checkPerfectNumber(int n) {
-        int sum=0;
-        for(int i=1;i<=n/2;i++){
-            if(n%i==0) sum+=i;
+    public boolean checkPerfectNumber(int num) {
+        if(num == 1) return false;
+        
+        int sum = 0;
+        for(int i = 2; i <= num/i; i++) {  
+            if(num % i == 0) {
+                sum = sum + (num/i) + i;
+            }
+            System.out.println(sum);
         }
-        if(n==sum) return true;
+        if(sum + 1 == num) return true;
         return false;
     }
 }
+
+
+
+// class Solution {
+//     public boolean checkPerfectNumber(int num) {
+//         return (num==6 || num==28 || num==496 || num==8128 || num==33550336);
+//     }
+// }
