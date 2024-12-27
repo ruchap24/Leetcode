dc4af45eceb3bf0f66939fbdf277246d4d8634a3
Rucha <160455987+ruchap24@users.noreply.github.com>
2024-12-27 19:45:31 +0530

    Time: 2 ms (100%), Space: 52.7 MB (88.38%) - LeetHub

--- 1063-best-sightseeing-pair/1063-best-sightseeing-pair.java
+++ 1063-best-sightseeing-pair/1063-best-sightseeing-pair.java
@@ -14,3 +14,17 @@ class Solution {
         return ans;
     }
 }
+
+
+// class Solution {
+//     public int maxScoreSightseeingPair(int[] arr) {
+//         int res = 0, max = 0;
+        
+//         for (int i = 0; i < arr.length; i++) {
+//             res = Math.max(res, max + arr[i] - i);
+//             max = Math.max(max, arr[i] + i);
+//         }
+
+//         return res;
+//     }
+// }
