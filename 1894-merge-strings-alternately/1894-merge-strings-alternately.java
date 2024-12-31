d242abbc4dc3780ce952a6663e846e5ecf984c93
Rucha <160455987+ruchap24@users.noreply.github.com>
2024-12-31 19:40:05 +0530

    Time: 1 ms (83.71%), Space: 41.6 MB (84.67%) - LeetHub

--- 1894-merge-strings-alternately/1894-merge-strings-alternately.java
+++ 1894-merge-strings-alternately/1894-merge-strings-alternately.java
@@ -1,43 +1,44 @@
 class Solution {
     public String mergeAlternately(String word1, String word2) {
-        char arr1[] = word1.toCharArray();
-        char arr2[] = word2.toCharArray();
-        int min = Math.min(arr1.length, arr2.length);
-
-        char arr[] = new char[arr1.length + arr2.length];
-        int sp = 0;
-        for (int i = 0; i < min; i++) {
-            arr[sp++] = arr1[i];
-            arr[sp++] = arr2[i];
-        }
-        if (arr1.length > min) {
-            for (int i = min; i < arr1.length; i++) {
-                arr[sp++] = arr1[i];
+        StringBuilder result = new StringBuilder();
+        int i = 0;
+        while (i < word1.length() || i < word2.length()) {
+            if (i < word1.length()) {
+                result.append(word1.charAt(i));
             }
-        } else if (arr2.length > min) {
-            for (int i = min; i < arr2.length; i++) {
-                arr[sp++] = arr2[i];
+            if (i < word2.length()) {
+                result.append(word2.charAt(i));
             }
+            i++;
         }
-        return new String(arr);
+        return result.toString();
     }
-}
+} 
 
 
+
+// using character array
 // class Solution {
 //     public String mergeAlternately(String word1, String word2) {
-//         StringBuilder result = new StringBuilder();
-//         int i = 0;
-//         while (i < word1.length() || i < word2.length()) {
-//             if (i < word1.length()) {
-//                 result.append(word1.charAt(i));
-//             }
-//             if (i < word2.length()) {
-//                 result.append(word2.charAt(i));
-//             }
-//             i++;
+//         char arr1[] = word1.toCharArray();
+//         char arr2[] = word2.toCharArray();
+//         int min = Math.min(arr1.length, arr2.length);
+
+//         char arr[] = new char[arr1.length + arr2.length];
+//         int sp = 0;
+//         for (int i = 0; i < min; i++) {
+//             arr[sp++] = arr1[i];
+//             arr[sp++] = arr2[i];
 //         }
-//         return result.toString();
+//         if (arr1.length > min) {
+//             for (int i = min; i < arr1.length; i++) {
+//                 arr[sp++] = arr1[i];
+//             }
+//         } else if (arr2.length > min) {
+//             for (int i = min; i < arr2.length; i++) {
+//                 arr[sp++] = arr2[i];
+//             }
+//         }
+//         return new String(arr);
 //     }
-// } 
-// O(n)
+// }
