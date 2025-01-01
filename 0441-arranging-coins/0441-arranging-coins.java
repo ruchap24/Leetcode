018d31181674ca4b3760ea8c5de6ec2dbc1d822d
Rucha <160455987+ruchap24@users.noreply.github.com>
2025-01-01 10:00:04 +0530

    Time: 1 ms (98.58%), Space: 41.2 MB (34.76%) - LeetHub

---
+++ 0441-arranging-coins/0441-arranging-coins.java
@@ -0,0 +1,20 @@
+class Solution{
+  public int arrangeCoins(int n) {
+        long start = 1, end = n;
+        long ans = 0, mid = 0;
+        
+        while(start <= end) {
+            mid = start + (end-start)/2;
+            
+            long coinCount = (mid*(mid+1))/2;
+                
+                if(coinCount <= n){
+                    ans = mid;
+                    start = mid+1;
+                } else {
+                    end = mid-1;
+                }
+        }
+        return (int)ans;
+    }
+}
