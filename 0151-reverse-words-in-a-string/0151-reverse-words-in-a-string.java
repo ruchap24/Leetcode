7c5e733038f8789fc2f6e6d07064eb9808cc5581
Rucha <160455987+ruchap24@users.noreply.github.com>
2025-01-07 17:51:55 +0530

    Time: 8 ms (44%), Space: 42.8 MB (83.64%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
@@ -1,15 +1,62 @@
+// Optimised code 
+// class Solution {
+//     public String reverseWords(String s) {
+//         int len = s.length();
+//         int i = len - 1;
+
+//         StringBuilder res = new StringBuilder();
+
+//         while (i >= 0) {
+//             // Skip trailing spaces
+//             while (i >= 0 && s.charAt(i) == ' ') {
+//                 i--;
+//             }
+
+//             // Find the start of the word
+//             int j = i;
+//             while (j >= 0 && s.charAt(j) != ' ') {
+//                 j--;
+//             }  // if we remove this while block then TLE will code......that's frustrating
+
+//             // Add the word to the result
+//             if (i > j) { // Only add if there's a valid word
+//                 if (res.length() > 0) {
+//                     res.append(' '); // Append a space between words
+//                 }
+//                 res.append(s.substring(j, i + 1));
+//             }
+
+//             i = j; // Move to the next word
+//         }
+
+//         return res.toString();
+//     }
+// }
+
+
+// \s+ 
+
 class Solution {
     public String reverseWords(String s) {
-        String[] words = s.split("\s+");
-        StringBuilder res = new StringBuilder();
-
-        for (int i = words.length - 1; i >= 0; i--) {
-            res.append(words[i]);
-            if (i != 0) {
-                res.append(" ");
-            }
-        }
-
-        return res.toString().trim();
+        String[] wordsArray = s.trim().split("\s+");
+            List<String> wordsList = new ArrayList<String>(Arrays.asList(wordsArray));
+            Collections.reverse(wordsList);
+                String reversed = String.join(" ", wordsList);
+                return reversed;
     }
 }
+
+
+// class Solution {
+//     public String reverseWords(String s) {
+//         String[] words=s.trim().split("\\s+");
+//         String result=new String();
+//         for(int i=words.length-1;i>=0;i--){
+//             result+=words[i];
+//             if(i!=0){
+//                 result+=" ";
+//             }
+//         }
+//         return result;
+//     }
+// }
